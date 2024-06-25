81c047cb49926dac1690445bba6af3a55f17c706
Randy <randy@randyrich.de>
2024-06-25 18:39:10 +0200

    added media queries and end date

--- resources/views/frontend/lessons/udemy-best-price.blade.php
+++ resources/views/frontend/lessons/udemy-best-price.blade.php
@@ -6,7 +6,7 @@
                 <x-lessons-nav />
                 <h2 style="margin-bottom: 5px">Check Out My Top Rated Udemy Courses</h2>
                 <h3 style="margin: 0">From Beginners to Advanced Levels, we got the course for you </h3>
-                <h2 class="red" style="margin-top: 5px">For Only 9.99 USD</h2>
+                <h2 class="red" style="margin-top: 5px">For Only 9.99 USD until June 30th</h2>
                 <div class="udemy-wrapper">
                     <div class="grid-container">
                         <div class="grid-item">
@@ -23,6 +23,20 @@
                             <a
                                 href="https://www.udemy.com/course/learn-travis-picking-from-scratch/?couponCode=RR-BESTTRAVIS-43"><img
                                     src="img/travis_best_price_thumb.jpg" alt="" /></a>
+                        </div>
+                        <div class="grid-item">
+                            <a
+                                href="https://www.udemy.com/course/the-amazing-guitar-solos-of-james-burton/?couponCode=RR-BESTBURTON-43"><img
+                                    src="img/burton_best_price_thumb.jpg" alt="" /></a>
+                        </div>
+                        <div class="grid-item">
+                            <a href="https://www.udemy.com/course/how-to-play-buddy-holly/?couponCode=RR-BESTBUDDY-43"><img
+                                    src="img/buddy_best_price_thumb.jpg" alt="" /></a>
+                        </div>
+                        <div class="grid-item">
+                            <a
+                                href="https://www.udemy.com/course/how-to-play-1950s-elvis-songs-just-like-scotty/?couponCode=RR-BESTSCOTTY-43"><img
+                                    src="img/scotty_best_price_thumb.jpg" alt="" /></a>
                         </div>
                         <div class="grid-item">
                             <a
@@ -51,20 +65,6 @@
                         <div class="grid-item">
                             <a href="https://www.udemy.com/course/rockabilly-rhythm-guitar/?couponCode=RR-BESTRHYTHM-43"><img
                                     src="img/rhythm_best_price_thumb.jpg" alt="" /></a>
-                        </div>
-                        <div class="grid-item">
-                            <a
-                                href="https://www.udemy.com/course/the-amazing-guitar-solos-of-james-burton/?couponCode=RR-BESTBURTON-43"><img
-                                    src="img/burton_best_price_thumb.jpg" alt="" /></a>
-                        </div>
-                        <div class="grid-item">
-                            <a href="https://www.udemy.com/course/how-to-play-buddy-holly/?couponCode=RR-BESTBUDDY-43"><img
-                                    src="img/buddy_best_price_thumb.jpg" alt="" /></a>
-                        </div>
-                        <div class="grid-item">
-                            <a
-                                href="https://www.udemy.com/course/how-to-play-1950s-elvis-songs-just-like-scotty/?couponCode=RR-BESTSCOTTY-43"><img
-                                    src="img/scotty_best_price_thumb.jpg" alt="" /></a>
                         </div>
                         <div class="grid-item">
                             <a href="https://www.udemy.com/course/gene-and-eddie/?couponCode=RR-BESTGENE-EDDIE-43"><img
